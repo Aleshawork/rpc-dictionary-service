4ffb09fa61f2e6d5f38ff82e249cc03965f87698
Alexey <borisov_alexey_2002@mail.ru>
2022-03-14 21:13:07 +0300

    правки

--- src/main/java/com/example/orm/config/DbConfig.java
+++ src/main/java/com/example/orm/config/DbConfig.java
@@ -4,9 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
-
 import javax.sql.DataSource;
 
 @Configuration
