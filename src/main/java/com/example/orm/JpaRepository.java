2649f3aba5a3036e50c90146331486f07e5014eb
Alexey <borisov_alexey_2002@mail.ru>
2022-03-26 15:25:33 +0300

    Настройка Liquibase

--- src/main/java/com/example/orm/JpaRepository.java
+++ src/main/java/com/example/orm/JpaRepository.java
@@ -1,13 +1,11 @@
 package com.example.orm;
 
-import org.hibernate.Filter;
+
 import org.hibernate.Session;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import java.util.Arrays;
-import java.util.Map;
 import java.util.function.Function;
 import java.util.logging.Logger;
 
---
+++ src/main/resources/db/changelog/changes/insert-information.sql
@@ -0,0 +1,2 @@
+insert into information(type,main_information)
+    values('PUBLIC_INFO','{"code":"UIY-890","source":"PUBLIC_INFO","data":"21.09.2021"}');
