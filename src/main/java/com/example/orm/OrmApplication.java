7f4efb6f56fdeea71750a062bd49d60347854821
Alexey <borisov_alexey_2002@mail.ru>
2022-03-30 22:37:13 +0300

    Liquibase changeSet

--- src/main/java/com/example/orm/OrmApplication.java
+++ src/main/java/com/example/orm/OrmApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 @SpringBootApplication
 public class OrmApplication {
 
